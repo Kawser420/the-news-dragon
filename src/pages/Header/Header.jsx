200712975b013eb00b23adeae8db19f6a956f60d
Kawser420 <muhammadkawser202@gmail.com>
2023-11-11 00:45:11 +0600

    the web site is not done. and will Handle this next time

--- src/pages/Header/Header.jsx
+++ src/pages/Header/Header.jsx
@@ -1,20 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import logo from '../../assets/logo.png';
 import moment from 'moment';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../providers/AuthProvider';
-import { FaUserCircle } from 'react-icons/fa';
+
 
 
 const Header = () => {
 
-    const { user } = useContext(AuthContext);
-
-
     return (
-        <Container>
+        <Container className='mt-4'>
             <div className="text-center">
                 <img src={logo} alt="" />
                 <p><small>Journalism Without Fear or Favour</small></p>
@@ -28,32 +23,6 @@ const Header = () => {
                     I can be a React component, multiple React components, or just some text...
                 </Marquee>
             </div>
-
-            <Navbar collapseOnSelect expand="lg" bg='light' variant='light' className="bg-body-tertiary">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#features">
-                                <Link to="/">Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
-                            <Nav.Link href="#pricing">Career</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            {user && <Nav.Link href="#deets"><FaUserCircle style={{ fontSize: '2.2em' }}></FaUserCircle></Nav.Link>}
-                            <Nav.Link eventKey={2} href="#memes">
-                                {user ?
-                                    <Button variant="dark">Logout</Button> :
-                                    <Link to='/login'>
-                                        <Button variant="dark">Login</Button>
-                                    </Link>
-                                }
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
         </Container>
     );
 };
