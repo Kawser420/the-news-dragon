b57015ce6b6d531d5d6e58d7dac41ce9edcc25ba
Kawser420 <muhammadkawser202@gmail.com>
2023-11-10 08:34:36 +0600

    Right side completed

---
+++ src/pages/Shared/Q-Zone/QZone.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import qZone1 from '../../../assets/qZone1.png';
+import qZone2 from '../../../assets/qZone2.png';
+import qZone3 from '../../../assets/qZone3.png';
+
+
+const Qzone = () => {
+    return (
+        <div>
+            <h3 className='mt-3'>Q-Zone</h3>
+            <div>
+                <img src={qZone1} alt="" />
+                <img src={qZone2} alt="" />
+                <img src={qZone3} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Qzone;
--- src/pages/Shared/RightNab.jsx
+++ src/pages/Shared/RightNab.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGithub, FaGoogle, FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa';
+import Qzone from './Q-Zone/QZone';
 
 const RightNab = () => {
     return (
         <div>
-            <h2>This is right nav</h2>
+            <h3>Login With</h3>
+            <div className='d-block'>
+                <Button variant="primary" className='mb-2'>< FaGoogle /> Login Google</Button>
+                <Button variant="secondary"><FaGithub></FaGithub> Login Github</Button>
+            </div>
+            <div className="mt-3">
+                <h3>Find Us On</h3>
+                <ListGroup className='mx-auto'>
+                    <ListGroup.Item><FaFacebook>facebook</FaFacebook></ListGroup.Item>
+                    <ListGroup.Item><FaTwitter>twitter</FaTwitter></ListGroup.Item>
+                    <ListGroup.Item><FaInstagram>instagram</FaInstagram></ListGroup.Item>
+                </ListGroup>
+            </div>
+            <Qzone></Qzone>
         </div>
     );
 };
